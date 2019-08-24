55f1f71afa0b64695d8c84632aaf4b152bbcc53a
itsmarkodowd <godfather_mark@hotmail.com>
2019-08-24 01:18:07 +0100

    Updates

---
+++ precache-manifest.6340a58e0d12e083e3470ee50669dfe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a218c199f4a419db20b504767e607f37",
+    "url": "/REACT-clothing-ecommerce/index.html"
+  },
+  {
+    "revision": "b47cc2713fe375d3fe89",
+    "url": "/REACT-clothing-ecommerce/static/css/main.5443cf96.chunk.css"
+  },
+  {
+    "revision": "edb6a1b98f1d7725a97f",
+    "url": "/REACT-clothing-ecommerce/static/js/2.72c67392.chunk.js"
+  },
+  {
+    "revision": "b47cc2713fe375d3fe89",
+    "url": "/REACT-clothing-ecommerce/static/js/main.c3cf4d00.chunk.js"
+  },
+  {
+    "revision": "0222330c6ada304d83c7",
+    "url": "/REACT-clothing-ecommerce/static/js/runtime~main.c3453f08.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/REACT-clothing-ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/REACT-clothing-ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
